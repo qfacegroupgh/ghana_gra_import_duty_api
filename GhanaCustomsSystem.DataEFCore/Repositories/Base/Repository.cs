a3ae51168390d22a44444a1e15861260e512826a
IAmQwame <mensahmensah@gmail.com>
2021-03-23 17:41:37 +0000

    Add Repository Implementation

---
+++ GhanaCustomsSystem.DataEFCore/Repositories/Base/Repository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using GhanaCustomsSystem.Domain.Extensions;
+using GhanaCustomsSystem.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace GhanaCustomsSystem.DataEFCore.Repositories.Base
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+	{
+		protected readonly GhanaCustomsSystemContext Context;
+
+		public Repository(GhanaCustomsSystemContext context)
+		{	
+
+			Context = context;
+		}
+
+
+		public async Task<TEntity> GetAsync(int id)
+		{
+			return await Context.Set<TEntity>().FindAsync(id);
+		}
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Context.Set<TEntity>().ToListAsync();
+        }
+
+		public Task<List<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+		{
+            return Context.Set<TEntity>().Where(predicate).ToListAsync();
+		}
+
+		public async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+		{
+			return await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+		}
+
+		public TEntity Add(TEntity entity)
+		{
+
+			Context.Set<TEntity>().Add(entity);
+			return entity;
+		}
+
+		public async Task<TEntity> AddWithCommitAsync(TEntity entity)
+		{
+
+			Context.Set<TEntity>().AddOrUpdate(entity);
+            await Context.SaveChangesAsync();
+			return entity;
+		}
+		public void Remove(TEntity entity)
+		{
+			Context.Set<TEntity>().Remove(entity);
+		}
+		public async Task RemoveWithCommitAsync(TEntity entity)
+		{
+			Context.Set<TEntity>().Remove(entity);
+            await Context.SaveChangesAsync();
+		}
+
+		
+		public async Task<TEntity> AddOrUpdateCommitAsync(TEntity entity)
+		{
+			
+				Context.Set<TEntity>().AddOrUpdate(entity);
+				await Context.SaveChangesAsync();
+				return entity;
+        }
+	}
+
+}
---
+++ GhanaCustomsSystem.DataEFCore/Repositories/VehicleTypeRepository.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GhanaCustomsSystem.DataEFCore.Repositories.Base;
+using GhanaCustomsSystem.Domain.Extensions;
+using GhanaCustomsSystem.Domain.Models;
+using GhanaCustomsSystem.Domain.Repositories;
+using GhanaCustomsSystem.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace GhanaCustomsSystem.DataEFCore.Repositories
+{
+    public class VehicleTypeRepository : Repository<VehicleType>, IVehicleTypeRepository
+    {
+        public VehicleTypeRepository(GhanaCustomsSystemContext context) : base(context)
+        {
+        }
+
+        private IQueryable<VehicleType> VehicleTypeQueryableBase()
+        {
+            return Context.VehicleTypes.AsNoTracking()
+                .Include(a => a.VehicleTypeTaxations)
+                .Include("VehicleTypeTaxations.Taxation");
+        }
+
+        public async Task<List<VehicleType>> GetVehicleTypes()
+        {
+            return await VehicleTypeQueryableBase().ToListAsync();
+
+        }
+
+        public async Task<VehicleType> GetVehicleType(int vehicleTypeId)
+        {
+            return await VehicleTypeQueryableBase()
+                .FirstOrDefaultAsync(a=>a.VehicleTypeId== vehicleTypeId);
+
+        }
+
+        public void InitData()
+        {
+            var vehicles = Context.VehicleTypes.ToList();
+
+            foreach (var vehicleType in vehicles)
+            {
+                var tax = Context.Taxations.ToList();
+
+
+                foreach (var taxation in tax)
+                {
+                    var vehicleTypeTaxation = new VehicleTypeTaxation
+                    {
+                        VehicleTypeId = vehicleType.VehicleTypeId,
+                        TaxationId =taxation.TaxationId,
+                        Value = taxation.DefaultValue
+                    };
+                    vehicleTypeTaxation.SetDefaultValue();
+
+                    Context.VehicleTypeTaxations.Add(vehicleTypeTaxation);
+                }
+              
+            }
+
+            Context.SaveChangesAsync();
+        }
+    }
+}

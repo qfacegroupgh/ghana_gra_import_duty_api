21346a3342c173af536fe77415691da515be9c11
IAmQwame <mensahmensah@gmail.com>
2021-03-23 17:01:20 +0000

    Add IUnitOfWork

---
+++ GhanaCustomsSystem.Domain/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using GhanaCustomsSystem.Domain.Repositories;
+
+namespace GhanaCustomsSystem.Domain
+{
+    public partial interface IUnitOfWork : IDisposable
+    {
+        int Commit();
+        Task<int> CommitAsync();
+
+        IVehicleTypeRepository VehicleTypes { get; }
+    }
+}

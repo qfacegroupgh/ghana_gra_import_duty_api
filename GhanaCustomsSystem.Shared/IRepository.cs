9d6e4315d699c6581c2b5b548579b4b276d1a2b2
IAmQwame <mensahmensah@gmail.com>
2021-03-23 17:10:07 +0000

    Add IRepository

---
+++ GhanaCustomsSystem.Shared/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GhanaCustomsSystem.Shared
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<List<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        TEntity Add(TEntity entity);
+        Task<TEntity> AddWithCommitAsync(TEntity entity);
+        Task<TEntity> AddOrUpdateCommitAsync(TEntity entity);
+        void Remove(TEntity entity);
+        Task RemoveWithCommitAsync(TEntity entity);
+    }
+
+}

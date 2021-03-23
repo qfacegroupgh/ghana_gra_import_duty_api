308ecfd9723e1e61714c9836bed07bb056986b2c
IAmQwame <mensahmensah@gmail.com>
2021-03-23 17:03:03 +0000

    Add IRepository

---
+++ GhanaCustomsSystem.Domain/Repositories/IVehicleTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GhanaCustomsSystem.Domain.Models;
+using GhanaCustomsSystem.Shared;
+
+namespace GhanaCustomsSystem.Domain.Repositories
+{
+    public interface IVehicleTypeRepository : IRepository<VehicleType>
+    {
+        void InitData();
+        Task<VehicleType> GetVehicleType(int vehicleTypeId);
+        Task<List<VehicleType>> GetVehicleTypes();
+
+    }
+}

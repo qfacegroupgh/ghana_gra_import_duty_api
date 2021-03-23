0a22a15946919882dfa1ff7931723314fd29b2c4
IAmQwame <mensahmensah@gmail.com>
2021-03-23 18:18:02 +0000

    get import duty of asset

--- GhanaCustomsApi/Controllers/CustomsController.cs
+++ GhanaCustomsApi/Controllers/CustomsController.cs
@@ -5,3 +5,45 @@
             var data =await _unitOfWork.VehicleTypes.GetVehicleTypes();
             return Ok(data);
         }
+        [HttpGet]
+        [Route("import-duty")]
+        public async Task<IActionResult> GetById(int id,decimal assetValue)
+        {
+            var data =await _unitOfWork.VehicleTypes.GetVehicleType(id);
+
+
+            decimal subTotal1 = 0;
+
+            var vehicleTypeTaxations = data.VehicleTypeTaxations;
+
+            var taxSubSection1 = vehicleTypeTaxations
+                .Where(a => a.Taxation.TaxationCalculationTypeId == (int)TaxationCalculationTypeEnum.SubTotol_1).ToList();
+            
+            var taxSubSection2 = vehicleTypeTaxations
+                .Where(a => a.Taxation.TaxationCalculationTypeId == (int)TaxationCalculationTypeEnum.SubTotol_2).ToList();
+
+            var result = new InvoiceViewModel{AssetValue= assetValue,VehicleTypeId = data.VehicleTypeId,Name = data.Name,InvoiceFooters = new List<InvoiceFooter>()};
+            result.InvoiceFooters.Add(new InvoiceFooter { Amount = assetValue, Rate = null, Descriptions = "SUB-TOTAL 1" });
+
+
+            foreach (var tax in taxSubSection1)
+            {
+                var footer = new InvoiceFooter {Amount = (tax.Value / 100) * assetValue, Rate = tax.Value,Descriptions = $"{tax.Taxation.Name} {tax.Value:N2}%"};
+                subTotal1 += footer.Amount;
+                result.InvoiceFooters.Add(footer);
+            }
+
+            var runTotal = subTotal1 + assetValue;
+            result.InvoiceFooters.Add(new InvoiceFooter { Amount = subTotal1, Rate = null, Descriptions = "SUB-TOTAL 2" });
+            decimal subTotal3 = 0;
+            foreach (var tax in taxSubSection2)
+            {
+                var footer = new InvoiceFooter { Amount = (tax.Value/100) * runTotal, Rate = tax.Value, Descriptions = $"{tax.Taxation.Name} {tax.Value:N2}%" };
+                subTotal3 += footer.Amount;
+                result.InvoiceFooters.Add(footer);
+            }
+            result.InvoiceFooters.Add(new InvoiceFooter { Amount = subTotal3, Rate = null, Descriptions = "SUB-TOTAL 3" });
+            result.TotalDutyDue = result.InvoiceFooters.Where(a => a.Rate == null).Sum(a=>a.Amount) - assetValue;
+
+            return Ok(result);
+        }

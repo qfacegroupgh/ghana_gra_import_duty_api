268b76feaa68534468c31279e5e0a3928d82fd22
IAmQwame <mensahmensah@gmail.com>
2021-03-24 03:08:44 +0000

    Add integration config

---
+++ GhanaCustomsSystem.IntegrationTest/Fixtures/ApiWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GhanaCustomsSystem.IntegrationTest.Fixtures
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<GhanaCustomsApi.Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(config =>
+            {
+                var integrationConfig = new ConfigurationBuilder()
+                    .AddJsonFile("integrationsettings.json")
+                    .Build();
+
+                config.AddConfiguration(integrationConfig);
+            });
+
+            // is called after the `ConfigureServices` from the Startup
+            //builder.ConfigureTestServices(services =>
+            //{
+            //    services.AddTransient<IWeatherForecastConfigService, WeatherForecastConfigStub>();
+            //});
+        }
+    }
+}
---
+++ GhanaCustomsSystem.IntegrationTest/Fixtures/IntegrationTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using Xunit;
+
+namespace GhanaCustomsSystem.IntegrationTest.Fixtures
+{
+    public abstract class IntegrationTestBase : IClassFixture<ApiWebApplicationFactory>
+    {
+        
+
+        protected readonly ApiWebApplicationFactory Factory;
+        protected readonly HttpClient Client;
+        protected readonly IConfiguration Configuration;
+
+        protected IntegrationTestBase(ApiWebApplicationFactory fixture)
+        {
+            Factory = fixture;
+            Client = Factory.CreateClient();
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("integrationsettings.json")
+                .Build();
+
+            // if needed, reset the DB
+            //_checkpoint.Reset(_configuration.GetConnectionString("SQL")).Wait();
+        }
+    }
+}
